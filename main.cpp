f6c97d74edea74fe3ca4326cf968ccef2d7a5a18
Collin Harrington <collin1234harr@gmail.com>
2024-02-14 20:36:02 +0000

    Edited to use pos/neg rpms

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include "mbed.h"
 #include "arm_book_lib.h"
 
-#define DUTY_MIN 0.025
-#define DUTY_MAX 0.125
+#define DUTY_FULL_SPEED_REVERSE 0.05
+#define DUTY_STOPPED            0.075
+#define DUTY_FULL_SPEED_FORWARD 0.10
+
+#define RPM_FULL_FORWARD        >40
+#define RPM_FULL_BACKWARDS      ~>40
 
 #define PERIOD   0.020
 
-#define DELAY_MS  1500
+#define DELAY_MS  5000
 
 PwmOut servo(PF_9);
 
@@ -16,13 +20,20 @@ DigitalOut led(LED2);
 int main()
 {
     servo.period(PERIOD);
+    servo.write(DUTY_STOPPED);
     while (true) {
         led = ON;
-        servo.write(DUTY_MIN);
+        servo.write(DUTY_FULL_SPEED_FORWARD);
         delay(DELAY_MS);
         led = OFF;
-        servo.write(DUTY_MAX);
+        servo.write(DUTY_STOPPED);
         delay(DELAY_MS);
+        // led = ON;
+        // servo.write(DUTY_FULL_SPEED_REVERSE);
+        // delay(DELAY_MS);
+        // led = OFF;
+        // servo.write(DUTY_STOPPED);
+        // delay(DELAY_MS);
     }
 }
 
